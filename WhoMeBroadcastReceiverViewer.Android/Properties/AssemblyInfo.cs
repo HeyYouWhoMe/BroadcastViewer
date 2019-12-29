5fca35a8462c0ed62aa514ef5e9d1f35b00477ac
Anthony Harrison <anthony@harrisonofthenorth.com>
2019-12-29 09:10:20 +0000

    Publish to Google Console Play Alpha Track Release as Version 1.0.1

--- WhoMeBroadcastReceiverViewer.Android/Properties/AssemblyInfo.cs
+++ WhoMeBroadcastReceiverViewer.Android/Properties/AssemblyInfo.cs
@@ -3,6 +3,12 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Android.App;
 
+
+#if DEBUG
+[assembly: Application(Debuggable=true)]
+#else
+[assembly: Application(Debuggable = false)]
+#endif
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
@@ -32,3 +38,5 @@ using Android.App;
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
 [assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+
+
